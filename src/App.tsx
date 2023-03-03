855f2c7e8d5e114a6edfbfe28b8d31aa9768a98d
Olek <dygons14@gmail.com>
2023-03-03 17:36:52 +0100

    budget-planner

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import Navigation from "./components/Navigation/Navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
+
 import HomePage from "./components/Pages/HomePage";
 import TransactionsPage from "./components/Pages/TransactionsPage";
 import CategoriesPage from "./components/Pages/CategoriesPage";
 import BudgetPage from "./components/Pages/BudgetPage";
 import BudgetContextProvider from "./context/BudgetContextProvider";
-
+import MissingPage from "./components/Pages/MissingPage";
 function App() {
   return (
     <BudgetContextProvider>
@@ -20,7 +21,7 @@ function App() {
           <Route path="/transactions" element={<TransactionsPage />}></Route>
           <Route path="/categories" element={<CategoriesPage />}></Route>
           <Route path="/budget" element={<BudgetPage />}></Route>
-          <Route path="*" element={<HomePage />}></Route>
+          <Route path="*" element={<MissingPage />}></Route>
         </Routes>
       </Router>
     </BudgetContextProvider>
---
+++ src/components/Pages/MissingPage.tsx
@@ -0,0 +1,7 @@
+import { Navigate } from "react-router-dom";
+
+const MissingPage = () => {
+  return <Navigate to="/home"></Navigate>;
+};
+
+export default MissingPage;
